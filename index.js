4eed49d8410fe861fcd059fe5334880070678486
Brian Hill <brian@daily.co>
2022-07-25 16:55:04 -0400

    Implement a commit linting action

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const githubToken = core.getInput('github_token');
+    const types = core.getInput('types').split(',');
+    if (github.context.eventName === 'pull_request') {
+      const octokit = github.getOctokit(githubToken);
+      const eventPayload = github.context.payload;
+      const commits = octokit.rest.pulls.listCommits({
+        owner: eventPayload.pull_request.repository.owner.login,
+        repo: eventPayload.pull_request.repository.name,
+        pull_number: eventPayload.number
+      });
+      if (!commits.some((c) => isConventional(c.message, types))) {
+        core.setFailed("Pull request requires a conventional commit message");
+      }
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function isConventional(message, types) {
+  return types.some(t => message.startsWith(`${t}: `));
+
 }
 
 run();
